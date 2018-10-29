7ed10f871e805bff62531d9c68d978153ac06e5f
bunnie <bunnie@alphamaxmedia.com>
2018-10-29 11:44:49 -0400

    start pulse audio, root user doesn't have it by default

--- run_exclave.sh
+++ run_exclave.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
+pulseaudio --start --log-target=syslog  # root user doesn't have pulse audio running...
+
+/home/pi/code/netv2-tests/bin/dut-off.sh  # make sure power is off to jig on start
+
 /home/pi/.cargo/bin/exclave -c /home/pi/code/netv2-tests
